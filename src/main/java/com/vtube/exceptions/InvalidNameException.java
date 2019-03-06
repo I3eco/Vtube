0921ffcb5d3b49b9e63617338ce1fafb7129f74f
VittorioBelorechki <47298173+VittorioBelorechki@users.noreply.github.com>
2019-03-06 18:03:08 +0200

    Age validation added!

--- src/main/java/com/vtube/exceptions/InvalidNameException.java
+++ src/main/java/com/vtube/exceptions/InvalidNameException.java
@@ -2,5 +2,35 @@ package com.vtube.exceptions;
 
 public class InvalidNameException extends Exception {
 
+	public InvalidNameException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidNameException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidNameException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidNameException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidNameException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 691773414457800246L;
+
 
 }
